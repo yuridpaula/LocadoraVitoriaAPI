ef7848c6cc0e2ceb339164cc799de1ce0c5eab2b
Yuri de Paula <yuri.oliveira@democrata.com.br>
2019-03-19 14:21:35 -0300

    initial commit

---
+++ src/main/java/locadoravitoria/LocadoraVitoriaApiApplication.java
@@ -0,0 +1,13 @@
+package locadoravitoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraVitoriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraVitoriaApiApplication.class, args);
+	}
+
+}
